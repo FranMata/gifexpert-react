3265abc4dd54185aa93735dca00edc6fc66956db
Francisco Mata <francisco.redondo23@gmail.com>
2020-09-28 15:56:18 -0600

    docs agregado

---
+++ docs/precache-manifest.b7925de7d0403494d3ceabe1af3a1aa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb5021192f17bf81a7a05c63df6c8724",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fecc72cb67c05c73d8be",
+    "url": "/static/css/main.c606d1ca.chunk.css"
+  },
+  {
+    "revision": "3104a6f8b92b1bd6f5cc",
+    "url": "/static/js/2.9f94bf50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9f94bf50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fecc72cb67c05c73d8be",
+    "url": "/static/js/main.95509c6f.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
